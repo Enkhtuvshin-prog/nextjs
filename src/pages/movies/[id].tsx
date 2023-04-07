37e0c7ccfdb369b2713104a6dd29e988dc84516c
Enkhtvshn <116425332+Enkhtvshn@users.noreply.github.com>
2023-04-07 13:08:25 +0800

    kk

--- src/pages/movies/[id].tsx
+++ src/pages/movies/[id].tsx
@@ -8,28 +8,34 @@ interface IProps {
 
 const Movie = ({ movie }: any) => {
   const router = useRouter();
-  console.log("mm=======>", movie);
+  // console.log("mm=======>", movie);
 
   if (router.isFallback) {
     return <div>Уншиж байна ...</div>;
   }
 
   return (
-    <div className="bg-blue-400 h-full">
+    <div className="bg-blue-400 ">
       <div className="container mx-auto">
-        <h1 className="text-violet-700 text-4xl"> {movie.title}</h1>
-        <div className="group">
-          <Image src={movie.poster} width={300} height={200} alt="poster" />
-          <p className="text-bold">
-            {movie.year} : {movie.imdb.rating}
-          </p>
+        <div className="grid grid-cols-4 p-5 bg-white rounded-lg">
+          <div className="group">
+            <Image src={movie.poster} width={300} height={200} alt="poster" />
+          </div>
+          <div className="px-3" >
+              <h1 className="text-violet-700 text-4xl"> {movie.title}</h1>
+              <p className="text-bold">
+              <span className="text-black font-medium text-lg">Year</span> :{movie.year} 
+              </p>
+              <p><span className="text-black font-medium text-lg">description:</span>   {movie.plot}</p> 
+         </div>
+
         </div>
       </div>
     </div>
   );
 };
 
-export async function getServerSideProps({ query }: any) {
+export async  function getServerSideProps({ query }: any) {
   const res = await fetch(`http://localhost:8009/movies/${query.id}`);
   const data = await res.json();
 
